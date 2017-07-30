b8f26bebf524046461d4184d77463078ba4f9f9b
APengu <mrdimka@yahoo.com>
2017-07-30 13:39:49 +0300

    quick update

--- js/categories/developers.js
+++ js/categories/developers.js
@@ -8,6 +8,7 @@ exec = function()
 	b.addLine("<font size = 6>How to add to your development environment</font>");
 	var buildGradle = b.addPane("Include following code into your build.gradle:");
 	buildGradle.addLine("<pre>repositories { \n	maven { \n		name = \"APengu Maven\" \n		url = \"https://raw.githubusercontent.com/APengu/PenguLibs/gh-pages\" \n	} \n}\n\ndependencies { \n	deobfCompile \"apengu:SolarFluxReborn:MCVERSION-VTVERSION:deobf\" \n}</pre>");
+	buildGradle.addLine("Where \"MCVERSION\" is Minecraft Version and \"VTVERSION\" is Vanilla Tech Version");
 }
 
 exec();
